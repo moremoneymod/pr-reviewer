1771d461f399dc88cdd232b4d6ae832a47e4e71c
moremoneymod <84ivan48@gmail.com>
2025-11-13 11:36:34 +0300

    add http configuration loader

--- internal/config/http.go
+++ internal/config/http.go
@@ -1,1 +1,57 @@
 package config
+
+import (
+	"errors"
+	"net"
+	"os"
+	"time"
+)
+
+const (
+	httpHostName        = "HTTP_HOST"
+	httpPortName        = "HTTP_PORT"
+	httpTimeoutName     = "HTTP_TIMEOUT"
+	httpIdleTimeoutName = "HTTP_IDLE_TIMEOUT"
+)
+
+type HTTPConfig struct {
+	host        string
+	port        string
+	timeout     time.Duration
+	idleTimeout time.Duration
+}
+
+func NewHTTPConfig() (*HTTPConfig, error) {
+	host := os.Getenv(httpHostName)
+	if len(host) == 0 {
+		return nil, errors.New("http host not found")
+	}
+	port := os.Getenv(httpPortName)
+	if len(port) == 0 {
+		return nil, errors.New("http port not found")
+	}
+
+	timeout, err := time.ParseDuration(os.Getenv(httpTimeoutName))
+	if err != nil {
+		return nil, err
+	}
+
+	idleTimeout, err := time.ParseDuration(os.Getenv(httpIdleTimeoutName))
+	if err != nil {
+		return nil, err
+	}
+
+	return &HTTPConfig{host, port, timeout, idleTimeout}, nil
+}
+
+func (cfg *HTTPConfig) Address() string {
+	return net.JoinHostPort(cfg.host, cfg.port)
+}
+
+func (cfg *HTTPConfig) IDLETimeout() time.Duration {
+	return cfg.idleTimeout
+}
+
+func (cfg *HTTPConfig) Timeout() time.Duration {
+	return cfg.timeout
+}
