d2d131e8346e1e069f1c057ba955c46b79dbd080
moremoneymod <84ivan48@gmail.com>
2025-11-13 11:37:49 +0300

    add pg configuration loader

--- internal/config/pg.go
+++ internal/config/pg.go
@@ -1,1 +1,29 @@
 package config
+
+import (
+	"errors"
+	"os"
+)
+
+const (
+	pgDSNEnvName = "PG_DSN"
+)
+
+type PGConfig struct {
+	dsn string
+}
+
+func NewPGConfig() (*PGConfig, error) {
+	dsn := os.Getenv(pgDSNEnvName)
+	if len(dsn) == 0 {
+		return nil, errors.New("pg dsn not found")
+	}
+
+	return &PGConfig{
+		dsn: dsn,
+	}, nil
+}
+
+func (cfg *PGConfig) DSN() string {
+	return cfg.dsn
+}
