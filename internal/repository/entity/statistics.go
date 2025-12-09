b2d42de69b7e0da79373d6a4a73f94ac82f84fb7
moremoneymod <84ivan48@gmail.com>
2025-12-09 16:26:04 +0300

    fix UserAssignmentStatistics userID field

--- internal/repository/entity/statistics.go
+++ internal/repository/entity/statistics.go
@@ -1,7 +1,7 @@
 package entity
 
 type UserAssignmentStatistics struct {
-	UserID            int    `db:"user_id"`
+	UserID            string `db:"user_id"`
 	Username          string `db:"username"`
 	TeamName          string `db:"team_name"`
 	TotalAssignments  int    `db:"total_assignments"`
