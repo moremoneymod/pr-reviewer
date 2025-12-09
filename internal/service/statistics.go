7866e7f058f553ed458ef78db8f8692fac510a54
moremoneymod <84ivan48@gmail.com>
2025-12-09 16:42:41 +0300

    add GetStatistics method

--- internal/service/statistics.go
+++ internal/service/statistics.go
@@ -6,72 +6,48 @@ import (
 	"log/slog"
 
 	"github.com/moremoneymod/pr-reviewer/internal/lib/logger/sl"
-	serv "github.com/moremoneymod/pr-reviewer/internal/service/domain"
+	"github.com/moremoneymod/pr-reviewer/internal/service/domain"
 )
 
-func (s *Service) GetStatistics(ctx context.Context) (*serv.Statistics, error) {
+func (s *Service) GetStatistics(ctx context.Context) (*domain.Statistics, error) {
 	const op = "internal.service.statistics.GetStatistics"
 
 	log := s.log.With(
 		slog.String("op", op))
 
-	allPRs, err := s.PRRepository.GetAllPR(ctx)
+	prStatistics, err := s.PRRepository.GetPRStatistics(ctx)
 	if err != nil {
-		log.Error("failed to get all PRs", sl.Err(err))
+		log.Error("failed to get pr statistics", sl.Err(err))
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	allTeams, err := s.TeamProvider.GetAllTeam(ctx)
+	userStatistics, err := s.UserProvider.GetUserStatistics(ctx)
 	if err != nil {
-		log.Error("failed to get all teams", sl.Err(err))
+		log.Error("failed to get user statistics", sl.Err(err))
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	statistics := &serv.Statistics{
-		UserAssignments: make(map[string]int),
-		PRAssignments:   make(map[string]int),
-		TeamStats:       make(map[string]serv.TeamStat),
+	teamStatistics, err := s.TeamProvider.GetTeamStatistics(ctx)
+	if err != nil {
+		log.Error("failed to get team statistics", sl.Err(err))
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	for _, pr := range allPRs {
-		statistics.TotalPRs++
-
-		switch pr.Status {
-		case serv.PRStatusOpen:
-			statistics.OpenPRs++
-		case serv.PRStatusMerged:
-			statistics.MergedPRs++
-		}
-
-		statistics.PRAssignments[pr.ID] = len(pr.Reviewers)
-
-		for _, reviewer := range pr.Reviewers {
-			statistics.UserAssignments[reviewer]++
-		}
+	userAssignmentsStat, err := s.UserProvider.GetUserAssignmentStatistics(ctx)
+	if err != nil {
+		log.Error("failed to get user assignment statistics", sl.Err(err))
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	for _, team := range allTeams {
-		teamStat := serv.TeamStat{
-			MemberCount:   len(team.Members),
-			ActiveMembers: 0,
-			PRCount:       0,
-		}
-
-		for _, member := range team.Members {
-			if member.IsActive {
-				teamStat.ActiveMembers++
-			}
-		}
-
-		for _, pr := range allPRs {
-			author, err := s.UserProvider.GetUser(ctx, pr.ID)
-			if err == nil {
-				if author.TeamName == team.Name {
-					teamStat.PRCount++
-				}
-			}
-		}
-		statistics.TeamStats[team.Name] = teamStat
+	statistics := domain.Statistics{
+		UserAssignments: userAssignmentsStat,
+		TotalPRs:        prStatistics.TotalPRs,
+		OpenPRs:         prStatistics.OpenPRs,
+		MergedPRs:       prStatistics.MergedPRs,
+		TotalTeams:      teamStatistics.TotalTeams,
+		TotalUsers:      userStatistics.TotalUsers,
+		ActiveUsers:     userStatistics.ActiveUsers,
 	}
-	return statistics, nil
+
+	return &statistics, nil
 }
