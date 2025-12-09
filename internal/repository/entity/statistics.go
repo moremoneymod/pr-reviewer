1f0f1b6afef05ed450e3c54d133269b1c4ae7c40
moremoneymod <84ivan48@gmail.com>
2025-12-09 14:35:03 +0300

    add some entities

--- internal/repository/entity/statistics.go
+++ internal/repository/entity/statistics.go
@@ -1,1 +1,25 @@
 package entity
+
+type UserAssignmentStatistics struct {
+	UserID            int    `db:"user_id"`
+	Username          string `db:"username"`
+	TeamName          string `db:"team_name"`
+	TotalAssignments  int    `db:"total_assignments"`
+	OpenAssignments   int    `db:"open_assignments"`
+	MergedAssignments int    `db:"merged_assignments"`
+}
+
+type PRStatistics struct {
+	TotalPRs  int `db:"total"`
+	OpenPRs   int `db:"open"`
+	MergedPRs int `db:"merged"`
+}
+
+type UserStatistics struct {
+	TotalUsers  int `db:"total"`
+	ActiveUsers int `db:"active"`
+}
+
+type TeamStatistics struct {
+	TotalTeams int `db:"total"`
+}
