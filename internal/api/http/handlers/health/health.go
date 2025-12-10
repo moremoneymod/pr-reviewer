9e7846615f91e348d27dfa95f7b7817ed54db08e
moremoneymod <84ivan48@gmail.com>
2025-12-10 11:57:38 +0300

    add health handler

--- internal/api/http/handlers/health/health.go
+++ internal/api/http/handlers/health/health.go
@@ -1,1 +1,22 @@
 package health
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
+func New(log *slog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "internal.api.http.handlers.health.New"
+
+		log := log.With(slog.String("op", op))
+
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, map[string]string{"status": "ok"})
+		log.Info("successfully health check")
+
+		return
+	}
+}
