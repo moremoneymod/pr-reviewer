9abc5443c1801492b4e8ad059bac5a42de9afe81
moremoneymod <84ivan48@gmail.com>
2025-12-13 09:38:32 +0300

    add validation struct tags

--- internal/api/http/dto/request/pr.go
+++ internal/api/http/dto/request/pr.go
@@ -1,16 +1,16 @@
 package request
 
 type PRCreateRequest struct {
-	PullRequestID   string `json:"pull_request_id"`
-	PullRequestName string `json:"pull_request_name"`
-	AuthorID        string `json:"author_id"`
+	PullRequestID   string `json:"pull_request_id" validate:"required,min=1"`
+	PullRequestName string `json:"pull_request_name" validate:"required,min=1"`
+	AuthorID        string `json:"author_id" validate:"required,min=1"`
 }
 
 type PRMergeRequest struct {
-	PullRequestID string `json:"pull_request_id"`
+	PullRequestID string `json:"pull_request_id" validate:"required,min=1"`
 }
 
 type PRReassignRequest struct {
-	PullRequestID string `json:"pull_request_id"`
-	OldUserID     string `json:"old_user_id"`
+	PullRequestID string `json:"pull_request_id" validate:"required,min=1"`
+	OldUserID     string `json:"old_user_id" validate:"required,min=1"`
 }
--- internal/api/http/dto/request/team.go
+++ internal/api/http/dto/request/team.go
@@ -1,12 +1,14 @@
 package request
 
+import _ "github.com/go-playground/validator/v10"
+
 type TeamRequest struct {
-	TeamName string              `json:"team_name"`
-	Members  []TeamMemberRequest `json:"members"`
+	TeamName string              `json:"team_name" validate:"required"`
+	Members  []TeamMemberRequest `json:"members" validate:"required,min=1,dive"`
 }
 
 type TeamMemberRequest struct {
-	UserID   string `json:"user_id"`
-	Username string `json:"username"`
-	IsActive bool   `json:"is_active"`
+	UserID   string `json:"user_id" validate:"required"`
+	Username string `json:"username" validate:"required"`
+	IsActive bool   `json:"is_active" validate:"required"`
 }
